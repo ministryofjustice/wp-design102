582fccf44aa98eb8f3eccf54fd7619934c97258c
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-08-25 16:39:29 +0100

    Make case studies carousel block dynamic. Configure post thumbnail size.

--- web/app/themes/design102/lib/setup.php
+++ web/app/themes/design102/lib/setup.php
@@ -35,6 +35,7 @@ function setup() {
   // http://codex.wordpress.org/Function_Reference/set_post_thumbnail_size
   // http://codex.wordpress.org/Function_Reference/add_image_size
   add_theme_support('post-thumbnails');
+  set_post_thumbnail_size(400, 230, true);
 
   // Enable post formats
   // http://codex.wordpress.org/Post_Formats
