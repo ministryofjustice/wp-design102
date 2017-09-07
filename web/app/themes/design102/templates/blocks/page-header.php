12d625e414f10beb0d477391df31d1879cd7a550
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-09-07 14:25:46 +0100

    Remove background image from page header blocks.

--- web/app/themes/design102/templates/blocks/page-header.php
+++ web/app/themes/design102/templates/blocks/page-header.php
@@ -1,19 +1,4 @@
-<?php
-
-use Roots\Sage\Extras;
-
-$jumbo = ( $fields['background_image'] !== false );
-
-$class = ['page-header-block'];
-if ($jumbo) {
-  $class[] = 'page-header-block--jumbo';
-  $styles = [
-    'background-image' => 'url("' . $fields['background_image']['sizes']['large'] . '")',
-  ];
-}
-
-?>
-<header class="<?= implode(' ', $class) ?>"<?php if (isset($styles)) echo ' style="' . Extras\style_attr($styles) . '"'; ?>>
+<header class="page-header-block">
   <h1><?= wptexturize($fields['heading']) ?></h1>
   <?php if (!empty($fields['subheading'])): ?>
     <div class="page-header-block__subheading"><?= wptexturize($fields['subheading']) ?></div>
