8f8df2b6256c303451d9ee1c873175743f0af3d5
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-10-02 22:07:54 +0100

    Begin adding job vacancies.

--- web/app/themes/design102/lib/shortcodes.php
+++ web/app/themes/design102/lib/shortcodes.php
@@ -2,16 +2,22 @@
 
 namespace Roots\Sage\Shortcodes;
 
+include 'shortcodes/JobVacancies.php';
+
+function trim_wrapper_paragraph($content) {
+  $content = trim($content);
+  $content = preg_replace('/^<\/p>/', '', $content);
+  $content = preg_replace('/<p>$/', '', $content);
+  return $content;
+}
+
 function testimonial($atts, $content = null) {
   $a = shortcode_atts([
     'person' => false,
     'org' => false,
   ], $atts);
 
-  $out = $content;
-  $out = trim($out);
-  $out = preg_replace('/^<\/p>/', '', $out);
-  $out = preg_replace('/<p>$/', '', $out);
+  $out = trim_wrapper_paragraph($content);
 
   $person = !empty($a['person']) ? wptexturize($a['person']) : false;
   $org = !empty($a['org']) ? wptexturize($a['org']) : false;
@@ -24,5 +30,7 @@ function testimonial($atts, $content = null) {
 
   return '<blockquote class="testimonial">' . $out . '</blockquote>';
 }
-
 add_shortcode('testimonial', __NAMESPACE__ . '\\testimonial');
+
+add_shortcode('jobs_list', __NAMESPACE__ . '\\JobVacancies::shortcode_jobs_list');
+add_shortcode('no_jobs', __NAMESPACE__ . '\\JobVacancies::shortcode_no_jobs');
