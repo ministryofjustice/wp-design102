04970c90dc21a41444c215a43700682a1ec4fd5b
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-10-09 15:44:38 +0100

    Only run ACF functions if the plugin is loaded.

--- web/app/themes/design102/lib/setup.php
+++ web/app/themes/design102/lib/setup.php
@@ -121,4 +121,6 @@ function admin_bar_bump() {
   echo str_replace('html {', 'html, .navbar.fixed-top {', $bump);
 }
 
-acf_add_options_page();
+if (function_exists('acf_add_options_page')) {
+  acf_add_options_page();
+}
