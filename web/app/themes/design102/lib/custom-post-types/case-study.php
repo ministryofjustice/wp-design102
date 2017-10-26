a8a863fedc90dde053e32b849337d0e775d98ba4
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-10-26 15:31:55 +0100

    Changes to case studies.
    - Remove case study categories, including front-end filtering functionality.
    - Display all case studies in 2 column design. (Remove first hero case study display.)

--- web/app/themes/design102/lib/custom-post-types/case-study.php
+++ web/app/themes/design102/lib/custom-post-types/case-study.php
@@ -7,8 +7,6 @@ class CaseStudy {
 
   public function __construct() {
     add_action('init', [$this, 'register_post_type']);
-    add_action('init', [$this, 'register_taxonomy']);
-    add_filter('query_vars', [$this, 'add_query_vars']);
   }
 
   public function register_post_type() {
@@ -32,36 +30,6 @@ class CaseStudy {
     ];
 
     register_post_type($this->postType, $args);
-  }
-
-  function register_taxonomy() {
-    register_taxonomy('project_category', $this->postType, [
-      'labels' => [
-        'name' => 'Project Categories',
-        'singular_name' => 'Category',
-        'search_items' => 'Search Categories',
-        'all_items' => 'All Categories',
-        'edit_item' => 'Edit Category',
-        'update_item' => 'Update Category',
-        'add_new_item' => 'Add New Category',
-        'new_item_name' => 'New Category',
-        'menu_name' => 'Categories',
-      ],
-      'rewrite' => false,
-      'hierarchical' => true,
-      'query_var' => true,
-    ]);
-  }
-
-  /**
-   * Filter to add 'category' query var
-   *
-   * @param array $vars
-   * @return array
-   */
-  function add_query_vars($vars){
-    $vars[] = 'category';
-    return $vars;
   }
 }
 
--- web/app/themes/design102/templates/case-study-card-large.php
+++
@@ -1,9 +0,0 @@
-<a href="<?php the_permalink(); ?>">
-  <div class="card text-white">
-    <?= the_post_thumbnail('4x3_medium', ['class' => 'card-img']); ?>
-    <div class="card-img-overlay-gradient">
-      <h2 class="card-title"><?php the_title(); ?></h2>
-      <p class="card-text"><?php the_field('client'); ?></p>
-    </div>
-  </div>
-</a>
