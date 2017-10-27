f7b5a4255048a5b33d4fa7ccc1cb15acc39733f4
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-10-27 15:43:59 +0100

    Changes to `text`, `definition-list` and `clients-list` blocks.
    `at-a-glance` block has changed name to `definition-list`. Fields will need to be migrated to the new ACF layout.

--- web/app/themes/design102/templates/blocks/clients-list.php
+++ web/app/themes/design102/templates/blocks/clients-list.php
@@ -1,37 +1,34 @@
-<div class="text-block">
-
-  <div class="text-block__content">
-
+<div class="clients-list-block">
+  <div class="client-list-block__intro">
     <h2><?= wptexturize($fields['heading']) ?></h2>
     <?= $fields['introductory_text'] ?>
+  </div>
 
-    <div class="clients-list">
-      <?php
+  <div class="clients-list">
+    <?php
 
-      foreach (array_slice($fields['clients'], 0, 4) as $i => $client) {
-        echo '<div class="clients-list__client">';
-        echo wp_get_attachment_image($client['logo']['id'], 'full');
-        echo '</div>';
-      }
+    foreach (array_slice($fields['clients'], 0, 4) as $i => $client) {
+      echo '<div class="clients-list__client">';
+      echo wp_get_attachment_image($client['logo']['id'], 'full');
+      echo '</div>';
+    }
 
-      ?>
-    </div>
+    ?>
+  </div>
 
-    <div class="clients-list expandable__content">
-      <?php
+  <div class="clients-list expandable__content">
+    <?php
 
-      foreach (array_slice($fields['clients'], 4) as $i => $client) {
-        echo '<div class="clients-list__client">';
-        echo wp_get_attachment_image($client['logo']['id'], 'full');
-        echo '</div>';
-      }
+    foreach (array_slice($fields['clients'], 4) as $i => $client) {
+      echo '<div class="clients-list__client">';
+      echo wp_get_attachment_image($client['logo']['id'], 'full');
+      echo '</div>';
+    }
 
-      ?>
-    </div>
-    <div class="expandable__trigger">
-      <a href="#" class="btn">Show all clients</a>
-    </div>
-
+    ?>
+  </div>
+  <div class="expandable__trigger">
+    <a href="#" class="btn">Show all clients</a>
   </div>
 
 </div>
