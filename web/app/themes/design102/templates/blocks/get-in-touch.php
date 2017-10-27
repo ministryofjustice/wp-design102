82464c8e3278299bf82d30c49a10a9f64b69b5e7
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-10-27 11:54:14 +0100

    Add background colour to Call To Action blocks.

--- web/app/themes/design102/templates/blocks/get-in-touch.php
+++ web/app/themes/design102/templates/blocks/get-in-touch.php
@@ -5,8 +5,7 @@ $button_text = get_field('get_in_touch_button_text', 'options');
 $button_link = get_field('get_in_touch_button_link', 'options');
 
 ?>
-
 <div class="call-to-action-block">
   <p><?= wptexturize($strapline) ?></p>
-  <a href="<?= $button_link ?>" class="btn btn-outline-primary btn-lg"><?= wptexturize($button_text) ?></a>
+  <a href="<?= $button_link ?>" class="btn btn-outline-white btn-lg"><?= wptexturize($button_text) ?></a>
 </div>
