f9b27163830b3490e28461dad2f1b68068cf9b5f
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-10-30 15:14:59 +0000

    A few small changes.
    - Use a default mouse pointer when hovering over team members
    - Change expansion text on team page to 'see more people'
    - Change the text on job description 'apply now' button

--- web/app/themes/design102/templates/blocks/job-vacancy-apply.php
+++ web/app/themes/design102/templates/blocks/job-vacancy-apply.php
@@ -1,4 +1,4 @@
 <div class="call-to-action-block">
-  <p>Want to work here?</p>
-  <a href="<?= $fields['application_url'] ?>" class="btn btn-outline-white btn-lg" target="_blank">Apply now</a>
+  <p>Interested in this role?</p>
+  <a href="<?= $fields['application_url'] ?>" class="btn btn-outline-white btn-lg" target="_blank">Find out more & apply</a>
 </div>
