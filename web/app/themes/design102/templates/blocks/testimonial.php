9042f4691ba660491771182d25dc9973d0bbd6f3
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-10-31 12:20:43 +0000

    Remove customisable background from testimonial block.

--- web/app/themes/design102/templates/blocks/testimonial.php
+++ web/app/themes/design102/templates/blocks/testimonial.php
@@ -1,29 +1,4 @@
-<?php
-
-use Roots\Sage\Extras;
-
-$style = [];
-$class = ['testimonial-block'];
-$btn_class = ['btn'];
-
-if (empty($fields['background_colour'])) {
-  $fields['background_colour'] = '#EB6600';
-}
-else {
-  $style['background-color'] = $fields['background_colour'];
-}
-
-if (Extras\contrastingTextColour($fields['background_colour']) == 'white') {
-  $class[] = 'text-white';
-  $btn_class[] = 'btn-outline-white';
-}
-else {
-  $btn_class[] = 'btn-outline-primary';
-}
-
-?>
-
-<div class="<?= Extras\class_attr($class) ?>"<?php if (!empty($style)) echo ' style="' . Extras\style_attr($style) . '"'; ?>>
+<div class="testimonial-block text-white">
   <blockquote class="testimonial">
     <div class="testimonial__quote">
       <?= $fields['quote'] ?>
@@ -43,9 +18,8 @@ else {
   if (!empty($fields['button_text']) && !empty($fields['button_link'])) {
     echo '<div class="testimonial-block__cta">';
     echo sprintf(
-      '<a href="%s" class="%s">%s</a>',
+      '<a href="%s" class="btn btn-outline-white">%s</a>',
       esc_attr($fields['button_link']),
-      esc_attr(implode(' ', $btn_class)),
       wptexturize($fields['button_text'])
     );
     echo '</div>';
