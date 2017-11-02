7a83d8150f867470caf3f3157203166e1143f07d
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2017-11-02 14:38:38 +0000

    Make footer content manageable from the Options page.

--- web/app/themes/design102/templates/footer.php
+++ web/app/themes/design102/templates/footer.php
@@ -8,23 +8,29 @@ use Roots\Sage\Extras;
   <div class="l-page-container">
     <div class="row">
         <div class="col-12 col-sm-6 col-md-3">
-            &copy; <?= date('Y') ?> design102
+          &copy; <?= date('Y') ?> design102
         </div>
         <div class="col-12 col-sm-6 col-md-3">
-            <a href="https://www.google.co.uk/maps/search/?api=1&query=DESIGN102&query_place_id=ChIJK_1a_dsEdkgR0s543WeZqoI">
-              102 Petty France<br/>
-              London SW1H 9AJ, UK
-            </a>
+          <?= get_field('footer_column_1', 'options') ?>
         </div>
         <div class="col-12 col-sm-6 col-md-3">
-            <a href="mailto:info@design102.gov.uk">info@design102.gov.uk</a><br/>
-            <a href="tel:+4402033346222">+44 (0)20 3334 6222</a>
+          <?= get_field('footer_column_2', 'options') ?>
         </div>
         <div class="col-12 col-sm-6 col-md-3">
-            Follow us on:
-            <a href="<?= get_field('social_profile_vimeo', 'options') ?>" target="_blank"><svg class="social-icon"><use xlink:href="<?= Extras\asset_url('images/social-icons.svg#vimeo') ?>" /></svg></a>
-            <a href="<?= get_field('social_profile_twitter', 'options') ?>" target="_blank"><svg class="social-icon"><use xlink:href="<?= Extras\asset_url('images/social-icons.svg#twitter') ?>" /></svg></a>
-            <a href="<?= get_field('social_profile_instagram', 'options') ?>" target="_blank"><svg class="social-icon"><use xlink:href="<?= Extras\asset_url('images/social-icons.svg#instagram') ?>" /></svg></a>
+          Follow us on:
+          <?php
+
+          $social_links = get_field('social_media_links', 'options');
+
+          foreach (['vimeo', 'twitter', 'instagram'] as $network) {
+            if (!empty($social_links[$network])) {
+              ?>
+              <a href="<?= esc_url($social_links[$network]) ?>" target="_blank"><svg class="social-icon"><use xlink:href="<?= Extras\asset_url("images/social-icons.svg#$network") ?>" /></svg></a>
+              <?php
+            }
+          }
+
+          ?>
         </div>
     </div>
   </div>
