7a3c6dcf7622a2cac6d38c9f8756910945a9cb03
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2018-03-07 17:18:32 +0000

    Add support for MP4 animations in blocks: Images, Hero Image, and Zoomable Images

--- web/app/themes/design102/templates/blocks/images.php
+++ web/app/themes/design102/templates/blocks/images.php
@@ -1,21 +1,28 @@
+<?php use Roots\Sage\Extras; ?>
+
 <div class="image-block">
   <?php
   $sizes = '(min-width: 1140px) 540px, (max-width: 575px) calc(100vw - 30px), calc(50vw - 30px)';
   ?>
   <div class="row">
     <?php foreach (['image_1', 'image_2'] as $k): ?>
+      <?php $f = $fields[$k]; ?>
       <div class="col-sm-6">
         <?php
 
-        $img_id = $fields[$k]['image']['ID'];
-        $caption = wptexturize($fields[$k]['caption']);
+        if ($f['type'] == 'image') {
+          $sizes = '(min-width: 960px) 930px, calc(100vw - 30px)';
+          $img_id = $f['image']['ID'];
+          echo wp_get_attachment_image($img_id, '4x3_medium', false, ['sizes' => $sizes]);
+        }
+        else {
+          echo Extras\mp4_animation($f['animation']['url']);
+        }
 
         ?>
 
-        <?= wp_get_attachment_image($img_id, '4x3_medium', false, ['sizes' => $sizes]) ?>
-
-        <?php if (!empty($caption)): ?>
-          <p class="caption"><?= $caption ?></p>
+        <?php if (!empty($f['caption'])): ?>
+          <p class="caption"><?= wptexturize($f['caption']) ?></p>
         <?php endif; ?>
 
       </div>
--- web/app/themes/design102/templates/blocks/zoomable-images.php
+++ web/app/themes/design102/templates/blocks/zoomable-images.php
@@ -1,24 +1,33 @@
+<?php use Roots\Sage\Extras; ?>
+
 <div class="image-block">
   <?php
   $sizes = '(min-width: 1140px) 540px, (max-width: 575px) calc(100vw - 30px), calc(50vw - 30px)';
   ?>
   <div class="row">
     <?php foreach (['image_1', 'image_2'] as $k): ?>
+      <?php $f = $fields[$k]; ?>
       <div class="col-sm-6">
         <?php
 
-        $img_id = $fields[$k]['image']['ID'];
-        $img_url = $fields[$k]['image']['url'];
-        $caption = wptexturize($fields[$k]['caption']);
+        if ($f['type'] == 'image') {
+          $sizes = '(min-width: 960px) 930px, calc(100vw - 30px)';
+          $img_id = $f['image']['ID'];
+          $img_url = $f['image']['url'];
+          $caption = wptexturize($f['caption']);
+
+          echo '<a href="' . esc_attr($img_url) . '" class="zoomable mfp-zoom" title="' . esc_attr($caption) . '">';
+          echo wp_get_attachment_image($img_id, '4x3_medium', false, ['sizes' => $sizes]);
+          echo '</a>';
+        }
+        else {
+          echo Extras\mp4_animation($f['animation']['url']);
+        }
 
         ?>
 
-        <a href="<?= esc_attr($img_url) ?>" class="zoomable mfp-zoom" title="<?= esc_attr($caption) ?>">
-          <?= wp_get_attachment_image($img_id, '4x3_medium', false, ['sizes' => $sizes]) ?>
-        </a>
-
-        <?php if (!empty($caption)): ?>
-          <p class="caption"><?= $caption ?></p>
+        <?php if (!empty($f['caption'])): ?>
+          <p class="caption"><?= wptexturize($f['caption']) ?></p>
         <?php endif; ?>
 
       </div>
