5f9c58b40d4149fc23a9b49518ffca7174146736
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2018-03-08 14:39:11 +0000

    Accessibility enhancement to avoid empty <h1> tags when Page Header block is used without a header.

--- web/app/themes/design102/templates/blocks/page-header.php
+++ web/app/themes/design102/templates/blocks/page-header.php
@@ -1,5 +1,7 @@
 <header class="page-header-block">
-  <h1 class="page-header-block__header"><?= wptexturize($fields['heading']) ?></h1>
+  <?php if (!empty($fields['heading'])): ?>
+    <h1 class="page-header-block__header"><?= wptexturize($fields['heading']) ?></h1>
+  <?php endif; ?>
   <?php if (!empty($fields['intro_text'])): ?>
     <div class="page-header-block__intro"><?= wptexturize($fields['intro_text']) ?></div>
   <?php endif; ?>
